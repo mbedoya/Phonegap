c352dc40e0374648f21e5f7da11948e8d47d3c60
mbedoya <mauricio.bedoya@gmail.com>
2014-04-10 15:11:26 -0500

    soap logic added

--- hello/www/js/index.js
+++ hello/www/js/index.js
@@ -6,7 +6,7 @@ $(document).ready( function(){
     // INICIO DE SESIÓN
 
     function finalizarInicioSesion(){
-        if (new Date().getSeconds() < 30){
+        if (new Date().getSeconds() < 60){
             $.mobile.changePage("#pageone", {transition: "none"});
         }else{
             $.mobile.changePage("#pageerroriniciosesion", {transition: "slideup"});
@@ -37,6 +37,30 @@ $(document).ready( function(){
         $.mobile.changePage("mapa.html", {transition: "none"});
     });
 
+    //Click al Boton Traer Datos
+    $("#pagedemo #botonTraerDatos").on("click", function(){
+
+        var url = 'http://wsf.cdyne.com/WeatherWS/Weather.asmx';
+        var metodo = 'GetWeatherInformation';
+        var mensaje =
+            '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/"> \
+            <soap:Body> \
+            <GetWeatherInformation xmlns="http://ws.cdyne.com/WeatherWS/" /> \
+            </soap:Body> \
+            </soap:Envelope>';
+
+        $.soap({
+            url:    url,
+            method: metodo,
+            data: mensaje,
+            success: function(msg) {
+                alert('exito');
+            },
+            error: function (xhr, ajaxOptions, thrownError) {
+                alert('error');
+            }
+        });
+    });
 
 });
 
